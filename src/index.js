b465d9c05d4f49460a75d0a0efb797e3a6c1aa4f
Pavel Chekalin <grokki91@gmail.com>
2023-01-12 21:14:17 +0300

    Add tests

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Demon from './app/Demon';
+import Magician from './app/Magician';
+import ChangeDamage from './app/ChangeDamage';
+
+const demon = new Demon();
+const magician = new Magician();
+const stonedObj = new ChangeDamage();
+console.log(demon);
+console.log(magician);
+console.log(stonedObj);
